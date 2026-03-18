a5e058de2eee8de3970a2e34375858e953b0cc06
Lime Consulting <limeconsulting@users.noreply.github.com>
2026-03-18 18:25:14 +0100

    Removed LEFT JOINT statements and included nom_commune on Views

--- sql/clickhouse_schema.sql
+++ sql/clickhouse_schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS immo;
 CREATE TABLE IF NOT EXISTS immo.m_price_m2_commune_month
 (
     code_commune String,
-    nom_commune String,	
+    nom_commune String,
     annee UInt16,
     mois UInt8,
     type_local LowCardinality(String),
@@ -20,77 +20,48 @@ ORDER BY (code_commune, type_local)
 SETTINGS index_granularity = 8192;
 
 
-CREATE TABLE IF NOT EXISTS immo.ref_commune
-(
-    code_commune String,
-    nom_commune String,
-    dep String
-)
-ENGINE = MergeTree
-ORDER BY (dep, code_commune)
-SETTINGS index_granularity = 8192;
-
-
 CREATE OR REPLACE VIEW immo.v_price_m2_commune_month AS
 SELECT
-    m.dep,
-    m.ingest_id,
-    m.code_commune,
-    r.nom_commune,
-    m.annee,
-    m.mois,
-    m.type_local,
-    m.nb_ventes,
-    m.prix_m2_median,
-    m.prix_m2_p25,
-    m.prix_m2_p75
-FROM immo.m_price_m2_commune_month AS m
-LEFT JOIN immo.ref_commune AS r
-    ON r.dep = m.dep
-   AND r.code_commune = m.code_commune;
-
+    dep,
+    ingest_id,
+    code_commune,
+    nom_commune,
+    annee,
+    mois,
+    type_local,
+    nb_ventes,
+    prix_m2_median,
+    prix_m2_p25,
+    prix_m2_p75
+FROM immo.m_price_m2_commune_month;
 
 CREATE OR REPLACE VIEW immo.v_price_m2_commune_year AS
 SELECT
-    m.dep,
-    m.ingest_id,
-    m.code_commune,
-    r.nom_commune,
-    m.annee,
-    m.type_local,
-    sum(m.nb_ventes_month) AS nb_ventes_year,
-    quantileExactWeighted(0.50)(m.prix_m2_median, m.nb_ventes_month) AS prix_m2_median_year,
-    quantileExactWeighted(0.25)(m.prix_m2_median, m.nb_ventes_month) AS prix_m2_p25_year,
-    quantileExactWeighted(0.75)(m.prix_m2_median, m.nb_ventes_month) AS prix_m2_p75_year
-FROM
-(
-    SELECT
-        dep,
-        ingest_id,
-        code_commune,
-        annee,
-        type_local,
-        prix_m2_median,
-        nb_ventes AS nb_ventes_month
-    FROM immo.m_price_m2_commune_month
-) AS m
-LEFT JOIN immo.ref_commune AS r
-    ON r.dep = m.dep
-   AND r.code_commune = m.code_commune
+    dep,
+    ingest_id,
+    code_commune,
+    nom_commune,
+    annee,
+    type_local,
+    sum(nb_ventes) AS nb_ventes_year,
+    quantileExactWeighted(0.50)(prix_m2_median, nb_ventes) AS prix_m2_median_year,
+    quantileExactWeighted(0.25)(prix_m2_median, nb_ventes) AS prix_m2_p25_year,
+    quantileExactWeighted(0.75)(prix_m2_median, nb_ventes) AS prix_m2_p75_year
+FROM immo.m_price_m2_commune_month
 GROUP BY
-    m.dep,
-    m.ingest_id,
-    m.code_commune,
-    r.nom_commune,
-    m.annee,
-    m.type_local;
+    dep,
+    ingest_id,
+    code_commune,
+    nom_commune,
+    annee,
+    type_local;
 
 
 CREATE OR REPLACE VIEW immo.v_price_m2_commune_year_bi AS
 SELECT
     dep,
-    nom_commune,
     code_commune,
+    nom_commune,
     ingest_id,
     annee,
     type_local,
